7ce1c1d285dc2650be2ae8a4ee61fa097bca3ddd
Taiga-Sekida <al21090@shibaura-it.ac.jp>
2025-10-09 21:34:53 +0900

    Revert "Merge pull request #1 from Taiga-Sekida/feature/add-hackday-pre"
    This reverts commit 6c57fa92c93f1de7aa0089a2ef89611c40abf4e0, reversing
    changes made to eeff4ad6485e6c243887e776e177338b514f1b1d.

--- frontend/app/page.js
+++
@@ -1,9 +0,0 @@
-export default function Page() {
-  return (
-    <>
-      <h1>Hello from Next.js (minimal app)</h1>
-      <p>This is a minimal page to allow the Dockerized Next dev server to start.</p>
-    </>
-  )
-}
-
